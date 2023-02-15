64f9a669487d34a981b18e421a4082d2d1f6d028
DM <bezrukovf@gmail.com>
2023-02-15 20:29:13 +0400

    Commit-35 ex-34

---
+++ JS/34.js
@@ -0,0 +1,10 @@
+function mutation(arr) {
+    let firstWord = arr[0].toLowerCase()
+    let secondWord = arr[1].toLowerCase()
+    for (let i = 0; i < secondWord.length; i++) {
+        if (firstWord.indexOf(secondWord[i]) < 0) return false
+    }
+    return true
+}
+
+mutation(["hello", "hey"]);
